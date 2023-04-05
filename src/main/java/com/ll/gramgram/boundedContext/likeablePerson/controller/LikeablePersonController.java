21af040a66addc711139f4e2871f5fd804ade5b0
AJays119 <moonjw119@gmail.com>
2023-04-05 16:10:06 +0900

    Test1

---
+++ src/main/java/com/ll/gramgram/boundedContext/likeablePerson/controller/LikeablePersonController.java
@@ -0,0 +1,80 @@
+package com.ll.gramgram.boundedContext.likeablePerson.controller;
+
+import com.ll.gramgram.base.rq.Rq;
+import com.ll.gramgram.base.rsData.RsData;
+import com.ll.gramgram.boundedContext.instaMember.entity.InstaMember;
+import com.ll.gramgram.boundedContext.likeablePerson.entity.LikeablePerson;
+import com.ll.gramgram.boundedContext.likeablePerson.service.LikeablePersonService;
+import com.ll.gramgram.boundedContext.likeablePerson.repository.LikeablePersonRepository;
+import com.ll.gramgram.boundedContext.member.entity.Member;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/likeablePerson")
+@RequiredArgsConstructor
+public class LikeablePersonController {
+    private final Rq rq;
+    private final LikeablePersonService likeablePersonService;
+
+    @GetMapping("/add")
+    public String showAdd() {
+        return "usr/likeablePerson/add";
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class AddForm {
+        private final String username;
+        private final int attractiveTypeCode;
+    }
+
+    @PostMapping("/add")
+    public String add(@Valid AddForm addForm) {
+        RsData<LikeablePerson> createRsData = likeablePersonService.like(rq.getMember(), addForm.getUsername(), addForm.getAttractiveTypeCode());
+
+        if (createRsData.isFail()) {
+            return rq.historyBack(createRsData);
+        }
+
+        return rq.redirectWithMsg("/likeablePerson/list", createRsData);
+    }
+
+    @GetMapping("/list")
+    public String showList(Model model) {
+        InstaMember instaMember = rq.getMember().getInstaMember();
+
+        // 인스타인증을 했는지 체크
+        if (instaMember != null) {
+            List<LikeablePerson> likeablePeople = likeablePersonService.findByFromInstaMemberId(instaMember.getId());
+            model.addAttribute("likeablePeople", likeablePeople);
+        }
+
+        return "usr/likeablePerson/list";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable("id") Integer id) {
+        Long instaMemberId = rq.getMember().getInstaMember().getId();
+        LikeablePerson likeablePerson = likeablePersonService.findById(id).get();
+        if (instaMemberId != likeablePerson.getFromInstaMember().getId()) {
+            return rq.historyBack("아이디가 일치하지 않습니다.");
+        }
+        this.likeablePersonService.delete(likeablePerson);
+        RsData deleteRs = likeablePersonService.delete(likeablePerson);
+        return rq.redirectWithMsg("/likeablePerson/list", "삭제되었습니다.");
+    }
+}
---
+++ src/test/java/com/ll/gramgram/boundedContext/member/controller/MemberControllerTests.java
@@ -0,0 +1,193 @@
+package com.ll.gramgram.boundedContext.member.controller;
+
+import com.ll.gramgram.boundedContext.member.controller.MemberController;
+import com.ll.gramgram.boundedContext.member.entity.Member;
+import com.ll.gramgram.boundedContext.member.service.MemberService;
+import jakarta.servlet.http.HttpSession;
+import jakarta.transaction.Transactional;
+import org.apache.catalina.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.*;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hibernate.Hibernate.get;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.assertj.core.api.Assertions.assertThat;
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+@ActiveProfiles("test")
+public class MemberControllerTests {
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private MemberService memberService;
+
+    /*@Test
+    @DisplayName("회원가입 폼")
+    void t001() throws Exception {
+        // WHEN
+        ResultActions resultActions = mvc
+                .perform(get("/member/join"))
+                .andDo(print()); // 크게 의미 없고, 그냥 확인용
+        // THEN
+        resultActions
+                .andExpect(handler().handlerType(MemberController.class))
+                .andExpect(handler().methodName("showJoin"))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect((ResultMatcher) content().string(containsString("""
+                        <input type="text" name="username"
+                        """.stripIndent().trim())))
+                .andExpect((ResultMatcher) content().string(containsString("""
+                        <input type="password" name="password"
+                        """.stripIndent().trim())))
+                .andExpect((ResultMatcher) content().string(containsString("""
+                        <input type="submit" value="회원가입"
+                        """.stripIndent().trim())));
+    }*/
+
+
+    @Test
+    @DisplayName("회원가입")
+    void Test002() throws Exception {
+        ResultActions resultActions = mvc
+                .perform(post("/member/join")
+                        .with(csrf())
+                        .param("username", "user10")
+                        .param("password", "1234")
+                )
+                .andDo(print());
+
+        resultActions
+                .andExpect(handler().handlerType(MemberController.class))
+                .andExpect(handler().methodName("join"))
+                .andExpect(status().is3xxRedirection());
+
+        Member member = memberService.findByUsername("user10").orElse(null);
+
+        assertThat(member).isNotNull();
+    }
+
+    @Test
+    @DisplayName("회원가입시에 올바른 데이터를 넘기지 않으면 400")
+    void Test003() throws Exception {
+        ResultActions resultActions = mvc
+                .perform(post("/member/join")
+                        .with(csrf()) // CSRF 키 생성
+                        .param("username", "user10")
+                )
+                .andDo(print());
+
+        // THEN
+        resultActions
+                .andExpect(handler().handlerType(MemberController.class))
+                .andExpect(handler().methodName("join"))
+                .andExpect(status().is4xxClientError());
+
+        // WHEN
+        resultActions = mvc
+                .perform(post("/member/join")
+                        .with(csrf()) // CSRF 키 생성
+                        .param("password", "1234")
+                )
+                .andDo(print());
+
+        // THEN
+        resultActions
+                .andExpect(handler().handlerType(MemberController.class))
+                .andExpect(handler().methodName("join"))
+                .andExpect(status().is4xxClientError());
+
+        // WHEN
+        resultActions = mvc
+                .perform(post("/member/join")
+                        .with(csrf()) // CSRF 키 생성
+                        .param("username", "user10" + "a".repeat(30))
+                        .param("password", "1234")
+                )
+                .andDo(print());
+
+        // THEN
+        resultActions
+                .andExpect(handler().handlerType(MemberController.class))
+                .andExpect(handler().methodName("join"))
+                .andExpect(status().is4xxClientError());
+
+        // WHEN
+        resultActions = mvc
+                .perform(post("/member/join")
+                        .with(csrf()) // CSRF 키 생성
+                        .param("username", "user10")
+                        .param("password", "1234" + "a".repeat(30))
+                )
+                .andDo(print());
+
+        // THEN
+        resultActions
+                .andExpect(handler().handlerType(MemberController.class))
+                .andExpect(handler().methodName("join"))
+                .andExpect(status().is4xxClientError());
+    }
+
+    /*@Test
+    @DisplayName("로그인 폼")
+    void t004() throws Exception {
+        // WHEN
+        ResultActions resultActions = mvc
+                .perform(get("/member/login"))
+                .andDo(print());
+
+        // THEN
+        resultActions
+                .andExpect(handler().handlerType(MemberController.class))
+                .andExpect(handler().methodName("showLogin"))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect((ResultMatcher) content().string(containsString("""
+                        <input type="text" name="username"
+                        """.stripIndent().trim())))
+                .andExpect((ResultMatcher) content().string(containsString("""
+                        <input type="password" name="password"
+                        """.stripIndent().trim())))
+                .andExpect((ResultMatcher) content().string(containsString("""
+                        <input type="submit" value="로그인"
+                        """.stripIndent().trim())));
+    }*/
+
+    @Test
+    @DisplayName("로그인 처리")
+    void Test005() throws Exception {
+        // WHEN
+        ResultActions resultActions = mvc
+                .perform(post("/member/login")
+                        .with(csrf()) // CSRF 키 생성
+                        .param("username", "user1")
+                        .param("password", "1234")
+                )
+                .andDo(print());
+
+        MvcResult mvcResult = resultActions.andReturn();
+        HttpSession session = mvcResult.getRequest().getSession(false);
+        SecurityContext securityContext = (SecurityContext)session.getAttribute("SPRING_SECURITY_CONTEXT");
+        User user = (User)securityContext.getAuthentication().getPrincipal();
+
+        assertThat(user.getUsername()).isEqualTo("user1");
+
+        // THEN
+        resultActions
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrlPattern("/**"));
+    }
+}
